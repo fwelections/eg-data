1a2e2f80b3e0935f5ccfd3250f9ce1514f5161ec
radproject <radhouanef@gmail.com>
2013-11-20 15:54:50 +0100

    scrapping presidential 2012 round 1 results

---
+++ Presidential 2012 Rnd1/script.py
@@ -0,0 +1,32 @@
+#download the 2012 egypt elections results from the official website 
+# http://presidential2012.elections.eg/index.php/round1-results has the results in both csv and xls formats but it doesn't show 
+#	the total registred voters and other information we might need , another thing is that to get the breakdown by district you need to go #	through every option in the select field to get individual files  .
+
+#  sudo apt-get install python-setuptools
+#  easy_install beautifulsoup4
+
+import urllib2
+from bs4 import BeautifulSoup
+import csv
+import os #for the se of wget command to download the files 
+source_url = "http://egelections-2011.appspot.com/Presidential2012/results1/index.html"
+# using urllib2 to read the remote html page
+html = urllib2.urlopen(source_url).read()
+#using BeautifulSoup library for pulling data out of HTML
+soup = BeautifulSoup(html)
+#gettting all the disticts
+districts_html =soup.find('select', id="cities")
+#print districts_html
+districts = districts_html.find_all('option')
+f = open('districts.csv', 'wb')
+writer = csv.writer(f,delimiter=',')
+#saving districts names and codes for possible use in joins or even to match file names with district names .
+for option in districts :
+	print option.text
+        print option.get('value')
+        writer.writerow([option.get('value'),option.text.encode('utf-8').strip()])
+	#linux command
+	csv_command="wget -P csv http://egelections-2011.appspot.com/Presidential2012/results1/csv/"+ option.get('value') + ".csv" 
+        excel_command="wget -P xls http://egelections-2011.appspot.com/Presidential2012/results1/excels/"+ option.get('value') + ".xls" 
+        os.system(csv_command)
+	os.system(excel_command)
